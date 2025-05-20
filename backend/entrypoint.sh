a4cf258960fd1bdb8391474f8b2e9443d72973f2
Hamidreza Hidary <thecastleoffalcon@gmail.com>
2025-05-20 21:29:43 +0330

    refactor(project-structure): migrate and restructure files under `backend`
    Body:
    - Renamed all modules, assets, and configuration files from root-level directories to a unified `backend` directory for improved organization and maintainability.
    - Added Docker configurations (`Dockerfile`, `docker-compose.yml`) for backend and nginx services to enable containerized deployment.
    - Deleted unused migration files related to corporate models like `Instrument`, `LiveStream`, etc., as part of a cleanup.
    - Introduced SSL certificate setup in nginx configuration to enforce secure communication (TLSv1.2+).
    - Updated entrypoint script for backend service to include necessary Django commands (`makemigrations`, `migrate`, `collectstatic`) and run the server.
    - Added new static uploads & cache directories in `backend/media/uploads`.
    
    This restructuring lays the groundwork for better scalability and adherence to best practices.

---
+++ backend/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# if any of the commands in your code fails the entire scripts fails.
+set -o errexit
+
+# exit if any of your variables is not set.
+set -o nounset
+
+set -x
+
+# Clear Python caches
+find . -name "*.pyc" -exec rm -f {} \;
+
+# Django commands
+python manage.py makemigrations
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+python manage.py runserver 0.0.0.0:8009
+# gunicorn --config config/gunicorn/prod.py kernel.wsgi:application
+
+exec "$@"
