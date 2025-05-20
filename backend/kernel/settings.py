6a75410272b1e4b6b2c7b6bc7e5bf396c7a21991
Hamidreza Hidary <thecastleoffalcon@gmail.com>
2025-05-20 22:56:14 +0330

    feat(live_stream, settings): enhance streaming compatibility and update configurations
    This commit addresses multiple enhancements:
    1. Adjusted `live_stream.detail.html` to refine HLS stream URL handling by correcting a slicing issue for `.m3u8` file extension detection.
    2. Updated `ALLOWED_HOSTS` and `CSRF_TRUSTED_ORIGINS` in Django `settings.py` to include `localhost`, improving local development environment compatibility.
    3. Changed port mapping in `docker-compose.yml` from `8009:8009` to `80:80` for better alignment with standard HTTP port usage.
    4. Updated `nginx-dev.conf` to switch the server's listening port from `8009` to `80`, ensuring consistency across Docker and Nginx configurations.
    
    These changes improve local development setup and ensure proper handling of live-stream URLs while following best practices for port management.

--- backend/kernel/settings.py
+++ backend/kernel/settings.py
@@ -32,11 +32,14 @@ SECRET_KEY = 'django-insecure-w2)c!*glw4yd!f#l%hnti@m(^dw_55&m23ycm(%g5)quig=7vk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost'
+    ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://127.0.0.1:8009',
-    'https://127.0.0.1:8009',  # Include HTTPS if applicable
+    'http://127.0.0.1',
+    'http://localhost',
 ]
 
 # Application definition
