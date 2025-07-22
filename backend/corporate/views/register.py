5fcef54dac8569c4722bed74370a06aeb5efcbcb
Hamidreza Hidary <thecastleoffalcon@gmail.com>
2025-07-22 17:45:09 +0330

    fix(register): update template path for registration view to align with new directory structure

--- backend/corporate/views/register.py
+++ backend/corporate/views/register.py
@@ -7,7 +7,7 @@ from corporate.forms.user import UserCreationForm
 
 
 class RegisterView(CreateView):
-    template_name = "auth/register.html"
+    template_name = "pages/auth/register.html"
     form_class = UserCreationForm
     success_url = reverse_lazy("pages:login")
 
